909471e260f553cab1abf8534bfb1a9022a4bf8d
Sylwia Grzegolec <s.grzegolec@gmail.com>
2018-08-01 20:49:53 +0200

    Added cors() to neos request.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ getNeosRequestUrl = (startDate, stopDate) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Put all API endpoints under '/api'
-app.get('/nasa/neos/:startDate/:stopDate', (req, res) => {
+app.get('/nasa/neos/:startDate/:stopDate', cors(), (req, res) => {
   request(
     getNeosRequestUrl(req.params.startDate, req.params.stopDate),
     {json: true},
