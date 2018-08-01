3b596d9c9f965e9e187d3478b3904ad3bdfc8e44
Sylwia Grzegolec <s.grzegolec@gmail.com>
2018-08-01 18:43:38 +0200

    Included initial client build.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ getNeosRequestUrl = (startDate, stopDate) => {
 };
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Put all API endpoints under '/api'
 app.get('/nasa/neos/:startDate/:stopDate', cors(), (req, res) => {
@@ -28,7 +28,7 @@ app.get('/nasa/neos/:startDate/:stopDate', cors(), (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
